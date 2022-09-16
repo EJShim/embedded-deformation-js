1530d7e05ac261727ac9b8d3b230d6fd369f2ec1
EJShim <ejshim@imagoworks.ai>
2022-09-16 09:26:02 +0900

    [feat] dynamic background color

--- src/App.svelte
+++ src/App.svelte
@@ -3,24 +3,29 @@ import { onMount } from "svelte";
 
 
 let m_rendererContainer;
+let m_background1 = "#FF0000";
 
 console.log("haha")
 
 onMount(()=>{
 
 	console.log(m_rendererContainer);
+
+	m_background1 = "#00AA00";
 })
 
 
 </script>
 
-<main bind:this={m_rendererContainer}>
+<main bind:this={m_rendererContainer}
+	style="--theme-background1:{m_background1}">
 	Renderer
 </main>
 
 <style>
 
-main{    
+main{
+	background-color: var(--theme-background1);
 	position : absolute;
 	top : 0;
 	left : 0;
