b2f3eb110c1ed14ee2269c05a8629e433b505445
EJShim <ejshim@imagoworks.ai>
2022-09-16 09:21:03 +0900

    [feat] first commit

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+import { onMount } from "svelte";
+
+
+let m_rendererContainer;
+
+console.log("haha")
+
+onMount(()=>{
+
+	console.log(m_rendererContainer);
+})
+
+
+</script>
+
+<main bind:this={m_rendererContainer}>
+	Renderer
+</main>
+
+<style>
+
+main{    
+	position : absolute;
+	top : 0;
+	left : 0;
+
+	width : 100%;
+	height : 100%;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+
+</style>
