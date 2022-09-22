b9df6b4e08f34964ed4492e7819b24a0af87be27
Elmo <elmo@clo3d.com>
2022-09-22 15:33:36 +0900

    [feat] change base address for gh-pages deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base : '/embedded-deformation-js/',
   plugins: [svelte()],
   build:{
     outDir:'docs'
